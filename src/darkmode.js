342197f9ffd8d71d378293d3977704ddb8efa6d8
maxamedsuhayb41@gmail.com <maxamedsuhayb41@gmail.com>
2023-09-22 15:17:47 +0300

    i updated more fetures on the website

---
+++ src/darkmode.js
@@ -0,0 +1,25 @@
+let isDarkMode = false;
+
+function toggleDarkMode() {
+  const sunIcon = document.getElementById('sunIcon');
+  const moonIcon = document.getElementById('moonIcon');
+  const body = document.body;
+
+  isDarkMode = !isDarkMode;
+
+  if (isDarkMode) {
+    // Switch to dark mode
+    sunIcon.classList.add('sun-opacity');
+    moonIcon.classList.remove('hidden');
+    moonIcon.classList.add('moon-opacity');
+    sunIcon.classList.remove('sun-opacity');
+    body.classList.add('dark-mode');
+  } else {
+    // Revert to light mode
+    moonIcon.classList.add('moon-opacity');
+    sunIcon.classList.remove('hidden');
+    sunIcon.classList.add('sun-opacity');
+    moonIcon.classList.remove('moon-opacity');
+    body.classList.remove('dark-mode');
+  }
+}
