25692bb77b46857c7dad05a2dcc48e83faa50bd5
maxamedsuhayb41@gmail.com <maxamedsuhayb41@gmail.com>
2023-10-31 15:04:44 +0300

    i added the peofile bar

---
+++ src/profile.js
@@ -0,0 +1,14 @@
+const profilePopup  =  document.querySelector('.profilePopup')
+
+
+function showProfilePopup(){
+  const profilePopup  =  document.querySelector('.profilePopup')
+
+
+  profilePopup.classList.remove('hidden')
+}
+function closeProfilePopup(){
+  const profilePopup  =  document.querySelector('.profilePopup')
+
+  profilePopup.classList.add('hidden')
+}
